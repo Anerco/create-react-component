d90a646095a03c1b0dacc37dca9abbaa4dcf99d7
Aner Cohen <aner.cohen@gmail.com>
2020-03-05 09:23:12 +0200

    functional component only

--- utils.js
+++ utils.js
@@ -1,17 +1,17 @@
-'use strict';
-const vscode = require('vscode');
-const fse = require('fs-extra');
-const fs = require('fs');
-const path = require('path');
-const pascalCase = require('change-case').pascalCase;
+"use strict";
+const vscode = require("vscode");
+const fse = require("fs-extra");
+const fs = require("fs");
+const path = require("path");
+const pascalCase = require("change-case").pascalCase;
 
-function logger(type, msg = '') {
+function logger(type, msg = "") {
   switch (type) {
-    case 'success':
+    case "success":
       return vscode.window.setStatusBarMessage(`Success: ${msg}`, 5000);
-    case 'warning':
+    case "warning":
       return vscode.window.showWarningMessage(`Warning: ${msg}`);
-    case 'error':
+    case "error":
       return vscode.window.showErrorMessage(`Failed: ${msg}`);
   }
 }
@@ -19,7 +19,7 @@ function logger(type, msg = '') {
 module.exports = {
   logger,
   generators: {
-    templatesDir: path.join(__dirname, '/templates'),
+    templatesDir: path.join(__dirname, "/templates"),
 
     createFile: (file, data) =>
       new Promise(resolve => {
@@ -28,7 +28,7 @@ module.exports = {
       }),
 
     resolveWorkspaceRoot: path =>
-      path.replace('${workspaceFolder}', vscode.workspace.rootPath),
+      path.replace("${workspaceFolder}", vscode.workspace.rootPath),
 
     createComponentDir: function(uri, componentName) {
       let contextMenuSourcePath;
@@ -52,35 +52,22 @@ module.exports = {
     createComponent: function(componentDir, componentName, type) {
       let templateFileName = this.templatesDir + `/${type}.template`;
 
-      const compName = pascalCase(componentName);
+      const compNamePascalCase = pascalCase(componentName);
+      const lowercaseComponent = componentName.toLowerCase();
 
       let componentContent = fs
         .readFileSync(templateFileName)
         .toString()
-        .replace(/{componentName}/g, compName);
+        .replace(/{ComponentName}/g, compNamePascalCase)
+        .replace(/{lowercaseComponentName}/g, lowercaseComponent);
 
-      let filename = `${componentDir}/${compName}.jsx`;
-
-      return this.createFile(filename, componentContent);
-    },
-
-    createTestFile: function(componentDir, componentName) {
-      let templateFileName = this.templatesDir + `/test.template`;
-
-      const compName = pascalCase(componentName);
-
-      let componentContent = fs
-        .readFileSync(templateFileName)
-        .toString()
-        .replace(/{componentName}/g, compName);
-
-      let filename = `${componentDir}/${compName}.test.jsx`;
+      let filename = `${componentDir}/${compNamePascalCase}.jsx`;
 
       return this.createFile(filename, componentContent);
     },
 
-    createPackageJSON: function(componentDir, componentName) {
-      let templateFileName = this.templatesDir + '/package.template';
+    createIndex: function(componentDir, componentName) {
+      let templateFileName = this.templatesDir + "/index.template";
 
       const compName = pascalCase(componentName);
       let indexContent = fs
@@ -88,21 +75,21 @@ module.exports = {
         .toString()
         .replace(/{componentName}/g, compName);
 
-      let filename = `${componentDir}/package.json`;
+      let filename = `${componentDir}/index.js`;
 
       return this.createFile(filename, indexContent);
     },
 
     createCSS: function(componentDir, componentName) {
-      let templateFileName = `${this.templatesDir}/sass.template`;
-
+      let templateFileName = `${this.templatesDir}/css.template`;
+      const lowercaseComponent = componentName.toLowerCase();
       const compName = pascalCase(componentName);
       let cssContent = fs
         .readFileSync(templateFileName)
         .toString()
-        .replace(/{componentName}/g, compName);
+        .replace(/{lowercaseComponentName}/g, lowercaseComponent);
 
-      let filename = `${componentDir}/${compName}.sass`;
+      let filename = `${componentDir}/${compName}.css`;
 
       return this.createFile(filename, cssContent);
     }
